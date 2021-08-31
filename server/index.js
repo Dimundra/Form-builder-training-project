6313a8c5c657c9619d62e02baaa1023b11fb6d98
Dmytro Maziar <dmazi@softserveinc.com>
2021-08-31 17:47:37 +0300

    Implemented server that returns built react app

--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3001,
+    port: 3002,
     host: '0.0.0.0',
   });
 
+  await server.register(require('@hapi/inert'));
+
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello Friend!';
+    path: '/{file*}',
+    handler: {
+      directory: {
+        path: '../client/build/',
+      },
     },
   });
 
   await server.start();
-  console.log('server has started on port 3001...');
+  console.log('server has started on port 3002...');
 };
 
 process.on('unhandledRejection', (err) => {
+  console.log(err);
   process.exit(1);
 });
 
