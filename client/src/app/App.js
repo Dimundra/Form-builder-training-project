ba2d6eab5e8334a9c06142104f5578c79e05d1f2
Dmytro Maziar <dmazi@softserveinc.com>
2021-09-02 14:00:10 +0300

    refactored code structure for App.js and index.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,35 +2,25 @@ import React from 'react';
 import LandingPage from './pages/landing-page/LandingPage.js';
 import SignUpPage from './pages/SignUpPage/SignUpPage.js';
 import SignInPage from './pages/SignInPage/SignInPage.js';
-import {
-  createTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
-
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
 
 class App extends React.Component {
   render() {
     return (
-        <ThemeProvider theme={theme}>
-          <Router>
-            <Switch>
-              <Route exact path='/'>
-                <LandingPage />
-              </Route>
-              <Route path='/sign-up'>
-                <SignUpPage />
-              </Route>
-              <Route path='/sign-in'>
-                <SignInPage />
-              </Route>
-            </Switch>
-          </Router>
-        </ThemeProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <LandingPage />
+          </Route>
+          <Route path='/sign-up'>
+            <SignUpPage />
+          </Route>
+          <Route path='/sign-in'>
+            <SignInPage />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 import App from './app/App.js';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
