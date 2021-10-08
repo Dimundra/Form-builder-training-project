04c1f616275ecbe59a3f1dc5ac77d535c06d65b5
Dmytro Maziar <dmazi@softserveinc.com>
2021-10-08 21:23:44 +0300

    implemented migrations

---
+++ server/migrations/20211008154323-create-user-model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('user', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nickname: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        validate: {
+          is: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('user');
+  },
+};
