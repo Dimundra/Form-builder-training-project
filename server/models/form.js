bf66cdf8cca1bfffdeb4c50eed3fd763ba693a67
Dmytro Maziar <dmazi@softserveinc.com>
2021-10-23 16:08:03 +0300

    rewrote implementation of the models with different api

--- server/models/form.js
+++ server/models/form.js
@@ -1,31 +1,21 @@
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class Form extends Model {}
-
-  Form.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        unique: true,
-      },
-      data: {
-        type: DataTypes.JSON,
-        allowNull: false,
-      },
+  const Form = sequelize.define('Form', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      timestamps: false,
-    }
-  );
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    data: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+  });
 
   Form.associate = ({ User }) => {
     Form.belongsTo(User);
--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,25 @@
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {}
-
-  User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      nickname: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        unique: true,
-      },
-      email: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING(60),
-        allowNull: false,
-      },
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      timestamps: false,
-    }
-  );
+    nickname: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+  });
 
   User.associate = ({ Form }) => {
     User.hasMany(Form);
